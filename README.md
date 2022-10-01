61624635be1ffef98cf8eff3bb99fe7023bb3460
wvbe <wybe@x-54.com>
2022-10-01 13:15:21 +0200

    Handle all node types, color all the things

--- README.md
+++ README.md
@@ -2,24 +2,33 @@
 
 This is a command-line utility that prettifies the XML coming in from your streamed input.
 
-## installation
+It is meant to make minified XML easier to read, for those of us who handle XML in a terminal
+some times.
 
-```
+## Installation
+
+To install Node package, run from anywhere:
+
+```sh
 npm i -g wvbe/prettify-xml
 ```
 
-## usage
+## Usage
 
 Pipe your lines into this sucker like:
 
-```
+```sh
 cat minified.xml | prettify-xml
 ```
+```sh
+curl http://rss.cnn.com/rss/edition.rss | prettify-xml
+```
+```sh
+unzip -p my-word-file.docx word/document.xml | prettify-xml
+```
 
-## todo
 
-- [ ] Namespaces
-- [ ] Coloring option
-- [ ] Comment nodes
-- [ ] Processing instruction nodes
-- [ ] CDATA nodes
+## Roadmap
+
+- [ ] Embellishing text data with quotes is optional
+- [ ] Respect `@xml:space`, optionally
--- prettify-xml.js
+++ prettify-xml.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
-const slimdom = require('slimdom');
+import { parseXmlDocument } from 'slimdom';
+import chalk from 'chalk';
 
 // Promise the input piped into this program
 async function getStreamedInputData() {
@@ -17,38 +18,130 @@ async function getStreamedInputData() {
 	return new Promise((resolve) => process.stdin.on('end', () => resolve(data)));
 }
 
-const INDENT = '  ';
+const colors = {
+	attributeNamespacePrefix: chalk.green,
+	attributeNodeName: chalk.green,
+	attributeSyntax: chalk.green,
+	attributeValue: chalk.greenBright,
+	cdataData: chalk.magenta,
+	cdataSyntax: chalk.magenta,
+	commentData: chalk.grey,
+	commentSyntax: chalk.grey,
+	elementNamespacePrefix: chalk.red,
+	elementNodeName: chalk.red,
+	elementSyntax: chalk.red,
+	processingInstructionData: chalk.blueBright,
+	processingInstructionName: chalk.blue,
+	processingInstructionSyntax: chalk.blue,
+	textData: chalk.white,
+	textSyntax: chalk.grey,
+};
 
-function toLines(node, prefix = '\n') {
-	if (node.nodeType === /* Node.ATTRIBUTE_NODE */ 2) {
-		return `${node.nodeName}="${node.value}"`;
+function attributeToString(node) {
+	const namespacePrefix = node.prefix;
+	return [
+		namespacePrefix && colors.attributeNamespacePrefix(namespacePrefix),
+		namespacePrefix && colors.attributeSyntax(':'),
+		colors.attributeNodeName(node.localName),
+		colors.attributeSyntax('="'),
+		colors.attributeValue(node.nodeValue),
+		colors.attributeSyntax('"'),
+	].join('');
+}
+
+function textToString(node) {
+	return colors.textSyntax(`"`) + colors.textData(node.nodeValue) + colors.textSyntax(`"`);
+}
+
+function elementToLines(node) {
+	const isSelfClosing = node.childNodes.length === 0;
+	const namespacePrefix = node.prefix;
+
+	const nodeName = [
+		namespacePrefix && colors.elementNamespacePrefix(namespacePrefix),
+		namespacePrefix && colors.elementSyntax(':'),
+		colors.elementNodeName(node.localName),
+	].join('');
+
+	const opening = [
+		colors.elementSyntax('<'),
+		nodeName,
+		...Array.from(node.attributes)
+			.map(attributeToString)
+			.map((line) => ` ${line}`),
+		(isSelfClosing && colors.elementSyntax(' /')) || '',
+		colors.elementSyntax('>'),
+	].join('');
+
+	if (isSelfClosing) {
+		return [opening];
 	}
-	if (node.nodeType === /* Node.TEXT_NODE */ 3) {
-		return `${prefix}"${node.nodeValue}"`;
+
+	return [
+		opening,
+		...Array.from(node.childNodes)
+			.reduce((flat, child) => [...flat, ...childToLines(child)], [])
+			.map((line) => `  ${line}`),
+		colors.elementSyntax('</') + nodeName + colors.elementSyntax('>'),
+	];
+}
+
+function commentToString(node) {
+	return (
+		colors.commentSyntax(`<!--`) + colors.commentData(node.nodeValue) + colors.commentSyntax(`-->`)
+	);
+}
+
+function cdataToString(node) {
+	return (
+		colors.cdataSyntax(`<![CDATA[`) + colors.cdataData(node.nodeValue) + colors.cdataSyntax(`]]>`)
+	);
+}
+
+function processingInstructionToString(node) {
+	return [
+		colors.processingInstructionSyntax(`<?`),
+		colors.processingInstructionName(node.nodeName),
+		node.nodeValue && colors.processingInstructionData(` ${node.nodeValue}`),
+		colors.processingInstructionSyntax(`?>`),
+	].join('');
+}
+
+function childToLines(node) {
+	switch (node.nodeType) {
+		case 1: // Element
+			return elementToLines(node);
+		case 2: // Attribute
+			return [attributeToString(node)];
+		case 3: // Text node
+			return [textToString(node)];
+		case 4: // CDATA
+			return [cdataToString(node)];
+		case 7: // Processing instruction
+			return [processingInstructionToString(node)];
+		case 8: // Comment
+			return [commentToString(node)];
+		case 9: // Comment
+			return Array.from(node.childNodes).reduce(
+				(flat, child) => [...flat, ...childToLines(child)],
+				[],
+			);
+		case 10: // Document type node
+		case 11: // Fragment
+		default:
+			throw new Error(
+				`Unsupported node type "${node.nodeType}", please submit an issue  @ https://github.com/wvbe/prettify-xml/issues/new`,
+			);
 	}
-	const indent = node.nodeType === /* Node.DOCUMENT_NODE */ 9 ? '' : INDENT;
-	const children = Array.from(node.childNodes || [])
-		.map((child) => toLines(child, prefix + indent))
-		.join('');
-	if (node.nodeType === /* Node.ELEMENT_NODE */ 1) {
-		const attrs = Array.from(node.attributes)
-			.map(toLines)
-			.map((str) => ` ${str}`)
-			.join(' ');
-		return `${prefix}<${node.nodeName + attrs}>${children}${prefix}</${node.nodeName}>`;
-	}
-	console.error(`Unmapped node of type ${node.nodeType}`);
-	return children;
 }
 
 (async () => {
 	const lines = await getStreamedInputData();
 	let dom;
 	try {
-		dom = slimdom.parseXmlDocument(lines);
+		dom = parseXmlDocument(lines);
 	} catch (error) {
 		throw new Error(`Your input XML could not be parsed: ${error.message}`);
 	}
-	const pretty = toLines(dom).trim();
-	console.log(pretty);
+	console.log(childToLines(dom).join('\n'));
 })().catch((error) => console.error(error.stack || error));
